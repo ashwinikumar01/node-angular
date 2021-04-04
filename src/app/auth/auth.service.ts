d00879ca9d2425f4c8c7590a2db03f2ef789f7df
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-04-04 21:51:46 +0530

    auth status listener added for detecting errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthModel } from './auth_model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private authStatusListener = new Subject<boolean>();
+
   constructor(private http: HttpClient, private router: Router) {}
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string, userDetails: string) {
     const authData: AuthModel = {
@@ -15,11 +22,14 @@ export class AuthService {
       password: password,
       userDetails: userDetails,
     };
-    this.http
-      .post('http://localhost:3000/api/user/signup', authData)
-      .subscribe((response) => {
+    this.http.post('http://localhost:3000/api/user/signup', authData).subscribe(
+      (response) => {
         console.log(response);
         this.router.navigate(['/login']);
-      });
+      },
+      (error) => {
+        this.authStatusListener.next(false);
+      }
+    );
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,13 +8,20 @@ import { AuthService } from '../auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   public hide = true;
   isLoading = false;
+  private authStatusSub: Subscription;
 
   constructor(private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        this.isLoading = false;
+      });
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
@@ -24,8 +32,12 @@ export class SignupComponent implements OnInit {
       this.authService.createUser(
         form.value.email,
         form.value.password,
-        form.value.details,
+        form.value.details
       );
     }
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
