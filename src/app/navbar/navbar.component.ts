8b0967dc3ab72117fb21b84ca4bddf3991c4067a
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-04-05 13:25:35 +0530

    few css changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   userIsAuthenticated = false;
