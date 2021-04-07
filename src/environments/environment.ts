94dafd74d1c00d7ae3c166c73398c58867f6525a
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-04-07 22:41:28 +0530

    few changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://online-web-auction.herokuapp.com/api',
 };
 
